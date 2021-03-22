8ee52c4d5ea5a042b9f1ce5a2cd79e5dd82dfdf8
ledima-MS <63517126+ledima-MS@users.noreply.github.com>
2021-03-22 08:56:27 +0100

    Project Commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PartyHub
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Frame_Partyhub.Content = new Content_Page.Dashboard();
+
+
+        }
+
+        private void Dashboard_Click(object sender, RoutedEventArgs e)
+        {
+            Frame_Partyhub.Content = new Content_Page.Dashboard();
+        }
+
+       
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# PartyHub-Wpf
+ Partyhub
