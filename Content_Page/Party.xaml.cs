6bd93925c5e391faf15ee3e990a640c13f2c6245
Manish Shrestha <manishestha@gmail.com>
2021-04-30 15:36:18 +0200

    Party Function Update

--- Content_Page/Party.xaml.cs
+++ Content_Page/Party.xaml.cs
@@ -20,9 +20,41 @@ namespace PartyHub.Content_Page
     /// </summary>
     public partial class Party : Page
     {
+        private CreatePartyWindow createPartySession = null;
+        private JoinPartyWindow joinPartySession = null;
         public Party()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (createPartySession == null)
+            {
+                createPartySession = new CreatePartyWindow();
+                createPartySession.Closed += SecondWindowClosed;
+                createPartySession.Show();
+
+            }
+
+
+        }
+        public void SecondWindowClosed(object sender, System.EventArgs e)
+        {
+            createPartySession = null;
+            joinPartySession = null;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (joinPartySession == null)
+            {
+                joinPartySession = new JoinPartyWindow();
+                joinPartySession.Closed += SecondWindowClosed;
+                joinPartySession.Show();
+
+            }
         }
     }
 }
