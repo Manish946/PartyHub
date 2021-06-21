f6df1323ee80322bb76c8ca557deb6f603df04f8
Manish Shrestha <manishestha@gmail.com>
2021-06-21 08:39:26 +0200

    last yupdate

--- LoginWindow.xaml.cs
+++ LoginWindow.xaml.cs
@@ -108,9 +108,9 @@ namespace PartyHub
         // Spotify Login Class where User information and tokens are stored. We can use it later to call API.
         public class SpotifyLogin
         {
-            public static string GetClient_Id { get; } = "e6e936eabb994075b4781bada6df5c4f";
+            public static string GetClient_Id { get; } = "XXXXXXXX";
 
-            public static string GetClient_Secret { get; } = "82c3d6f6c406450dac8c81a1baeeb40f";
+            public static string GetClient_Secret { get; } = "XXXXXXXXX";
 
             public static string GetRedirect_Uri { get; } = "http://localhost:80";
 
